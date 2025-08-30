dd58afd44a9404f02bbb857f446d565f96acb01d
Chavali J S R Vinayaka Gopala Krishna <147472515+VinayakaGopalaKrishnA123@users.noreply.github.com>
2025-08-30 20:25:14 +0530

    [36] [Time Beats: 16.01%] [Memory Beats: 35.57%] - LeetPush

---
+++ Valid-Sudoku.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        vector<unordered_set<char>> row(9), col(9), sq(9);
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
+                char ch=board[i][j];
+                if(ch=='.') continue;
+                int s=(i/3)*3+(j/3);
+                if(row[i].count(ch)||col[j].count(ch)||sq[s].count(ch)) return false;
+                row[i].insert(ch);
+                col[j].insert(ch);
+                sq[s].insert(ch);
+            }
+        }
+        return true;
+    }
+};
